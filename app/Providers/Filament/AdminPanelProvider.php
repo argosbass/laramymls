6d364b2486a88251d4e406332ebc30e2de4ced9d
Danny Mora <dmora@dc.cyberfuel.com>
2026-02-25 11:04:40 -0600

    DMR - Sidebar collapsible

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -59,6 +59,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
+            ->sidebarCollapsibleOnDesktop()
             ->collapsibleNavigationGroups(false);
     }
 }
